3f31d9e6b9b4c00b5abd7456b1506068cabcce58
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2018-03-26 14:43:47 -0500

    Added initial impl. of the RedisMonitor class

---
+++ rtdb_sync_pub/redis_monitor.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2018, Sorbotics LLC.
+# All rights reserved.
+
+
+__all__ = ["RedisMonitor"]
+
+
+class RedisMonitor:
+    """Monitor Redis and notify changes."""
+
+    def monitor(self):
+        """Produce an iterable of changes on the Redis database."""
+        return iter([])
