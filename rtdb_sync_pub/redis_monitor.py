512679da2a297c120ad0391a83902a3a2867a29a
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2018-04-10 11:20:19 -0500

    Implemented the RedisMonitor class

--- rtdb_sync_pub/redis_monitor.py
+++ rtdb_sync_pub/redis_monitor.py
@@ -10,6 +10,32 @@ __all__ = ["RedisMonitor"]
 class RedisMonitor:
     """Monitor Redis and notify changes."""
 
+    def __init__(self, connection_pool):
+        self.connection_pool = connection_pool
+        self.connection = None
+
+    def __del__(self):
+        try:
+            self.reset()
+        except:
+            pass
+
+    def reset(self):
+        if self.connection:
+            self.connection_pool.release(self.connection)
+            self.connection = None
+
+    def read_response(self):
+        return self.connection.read_response()
+
+    def listen(self):
+        while True:
+            yield self.read_response()
+
     def monitor(self):
         """Produce an iterable of changes on the Redis database."""
-        return iter([])
+        if self.connection is None:
+            self.connection = self.connection_pool.get_connection(
+                'monitor', None)
+        self.connection.send_command("monitor")
+        return self.listen()
