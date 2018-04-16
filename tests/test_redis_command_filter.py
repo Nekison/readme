b2c317ca7f450ee73e8c2c1770f0693d42843a76
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2018-04-16 16:04:04 -0500

    Added a new test case for the redis_command_filter module

---
+++ tests/test_redis_command_filter.py
@@ -0,0 +1,27 @@
+"""Provide test cases for rtdb_sync_pub.redis_command_filter module.
+
+Copyright (c) 2018, Sorbotics LLC.
+All rights reserved.
+"""
+
+import os
+import unittest
+
+from rtdb_sync_pub import redis_command_filter
+
+
+class TestFilterCommands(unittest.TestCase):
+    """filter_commands function test case."""
+
+    def test_return_iterable(self):
+        """Test that monitor() method produce an iterable."""
+        filter_commands_result = redis_command_filter.filter_commands([])
+
+        try:
+            iter(filter_commands_result)
+        except TypeError:
+            self.fail("filter_commands() return value is not iterable")
+
+
+if __name__ == '__main__':
+    unittest.main()
