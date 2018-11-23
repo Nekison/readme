dc475ee6dfd43657484c1e1e4050f76299bc4981
omenlogo <omarlg@unica.cu>
2018-11-22 23:09:29 -0500

    Declare on_disconnect callback to handle mqtt broker disconnection

--- rtdb_sync_pub/utils/mqtt.py
+++ rtdb_sync_pub/utils/mqtt.py
@@ -1,9 +1,45 @@
 """Module to provide mqtt utilities."""
 
-# Third party modules
+# Standard library imports
+import sys
+import time
+
+# Third party imports
 import paho.mqtt.client as mqtt
 
+# Local application imports
+from .exceptions import MqttBrokerIsDown
+
+
 __all__ = ["create_client"]
+
+
+def _on_disconnect(args, exec_queue):
+    """Manage disconnect event from mqtt broker."""
+    timeout_time = args.limit_time
+
+    def inner_on_disconnect(client, userdata, result):
+        start_time = time.time()
+
+        while True:
+            try:
+                print("Trying to reconnect mqtt client to {} broker"
+                      .format(args.mqtt_host))
+
+                client.reconnect()
+                break
+            except ConnectionRefusedError as error:
+                elapsed_time = time.time() - start_time
+
+                if elapsed_time > timeout_time:
+                    client.loop_stop()
+                    exec_queue.put("BROKER_DOWN")
+                    sys.exit(1)
+                else:
+                    time.sleep(1)
+                    continue
+
+    return inner_on_disconnect
 
 
 def _on_connect(client, userdata, flags, rc):
@@ -22,12 +58,14 @@ def _on_publish(client, userdata, result):
     print("MQTT Message Published with result code {}".format(str(result)))
 
 
-def create_client(args):
+def create_client(args, exec_queue):
     client = mqtt.Client()
 
     # setup client settings
     client.on_connect = _on_connect
     client.on_publish = _on_publish
+    client.on_disconnect = _on_disconnect(args, exec_queue)
+
     client.enable_logger()
 
     if args.mqtt_port != 1883:
